1654f83179804fc5ab5097bb094f942634a0ea63
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-20 15:20:47 -0600

    Enviando datos del hijo al padre con @Ouput

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,11 +7,22 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImgComponent  implements OnInit{
 
-  @Input() img: string = 'valor inicial';
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = 'https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg';
 
   constructor() {}
 
   ngOnInit(): void {
+
+  }
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+  imgLoaded() {
+    console.log("Image Cargada");
+    this.loaded.emit(this.img);
 
   }
 
