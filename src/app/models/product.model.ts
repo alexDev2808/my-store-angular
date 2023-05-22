b10a8f702c559d6733c73cdd36ab36f082fa4198
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-22 09:18:38 -0600

    Comenzando con el consumo de APIs REST, utilizando el metodo get

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,10 +1,17 @@
-// Tipar productos
 
+export interface Category {
+  id: string,
+  name: string
+}
+
+
+
+// Tipar productos
 export interface Product {
   id: string,
   title: string,
   price: number,
-  image: string,
+  images: string[],
   description: string,
-  category: string
+  category: Category
 }
